afdcfda9606ed592264268a928b93c2f0b6d3bdc
dittmptr27 <adityamuhamadputra@gmail.com>
2023-11-05 20:00:16 +0700

    add config ad for nativeAdUnitId

--- lib/constans/ad_helper.dart
+++ lib/constans/ad_helper.dart
@@ -30,4 +30,13 @@ class AdHelper {
       throw UnsupportedError('Unsupported platform');
     }
   }
+
+  static String get nativeAdUnitId {
+    if (Platform.isAndroid) {
+      return 'ca-app-pub-3940256099942544/2247696110';
+    } else if (Platform.isIOS) {
+      return 'ca-app-pub-3940256099942544/3986624511';
+    }
+    throw UnsupportedError("Unsupported platform");
+  }
 }
